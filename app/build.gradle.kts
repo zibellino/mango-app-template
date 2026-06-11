1a175808c4e20e1af3ecc7e67b2d447ad85121f3
Szabolcs <szabolcs.pall@aol.com>
2026-06-11 14:19:27 +0200

    Add androidx.appcompat dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,5 +45,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
     debugImplementation(libs.androidx.ui.tooling)
 }
